2b5710769d2c293e56e6df060e8e251c940e1ed9
tangc1986 <tangc1986@gmail.com>
2015-09-09 23:55:11 +0800

    新增重定向脚本

---
+++ wxpython/redirect.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import wx
+import sys
+
+
+class Frame(wx.Frame):
+
+    def __init__(self, parent, id, title):
+        print "Frame __init__"
+        wx.Frame.__init__(self, parent, id, title)
+
+class App(wx.App):
+
+    def __init__(self, redirect=True, filename=None):
+        print "App __init__"
+        wx.App.__init__(self, redirect, filename)
+
+    def OnInit(self):
+        print "OnInit"      # 输出到stdout
+        self.frame = Frame(parent=None, id=-1, title='Startup')     # 创建框架
+        self.frame.Show()
+        self.SetTopWindow(self.frame)
+        print >> sys.stderr, "A pretend error message"      # 输出到stderr
+        return True
+
+    def OnExit(self):
+        print "OnExit"
+
+if __name__ == '__main__':
+    app = App(True, "output")        #1 文本重定向从这开始
+    print "before MainLoop"
+    app.MainLoop()      #2 进入主事件循环
+    print "after MainLoop"
