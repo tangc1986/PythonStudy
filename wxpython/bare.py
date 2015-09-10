e87e9a3ca194710ae2e00abac6f702b8017e72e4
tangc1986 <tangc1986@gmail.com>
2015-09-11 00:40:58 +0800

    新增insertFrame.py

--- wxpython/bare.py
+++ wxpython/bare.py
@@ -5,7 +5,7 @@ import wx               #1
 class App(wx.App):      #2
 
     def OnInit(self):   #3
-        frame = wx.Frame(parent=None, id=-1, title='Bare')
+        frame = wx.Frame(parent=None, id=-1, title='Bare', style=wx.DEFAULT_FRAME_STYLE|wx.FRAME_TOOL_WINDOW)
         frame.Show()
         return True
 
