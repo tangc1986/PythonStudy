429446d6fcf4e502eba4070544403454e23d6057
tangc1986 <tangc1986@gmail.com>
2015-09-16 23:32:10 +0800

    重构以后

--- wxpython/badExample.py
+++ wxpython/badExample.py
@@ -9,51 +9,69 @@ class RefactorExample(wx.Frame):
                 size=(340, 200))
         panel = wx.Panel(self, -1)
         panel.SetBackgroundColour("White")
-        prevButton = wx.Button(panel, -1, "<< PREV", pos=(80, 0))
-        self.Bind(wx.EVT_BUTTON, self.OnPrev, prevButton)
-        nextButton = wx.Button(panel, -1, "NEXT >>", pos=(160, 0))
-        self.Bind(wx.EVT_BUTTON, self.OnNext, nextButton)
         self.Bind(wx.EVT_CLOSE, self.OnCloseWindow)
+        self.createMenuBar()
+        self.createButtonBar(panel)
+        self.createTextFields(panel)
 
+    def menuData(self):
+        return (("&File", ("&Open", "Copy in status bar", self.OnOpen),
+                          ("&Quit", "Quit", self.OnCloseWindow)),
+                ("&Edit", ("&Copy", "Copy", self.OnCopy),
+                          ("C&ut", "Cut", self.OnCut),
+                          ("Paste", "Paste", self.OnPaste),
+                          ("", "", ""),
+                          ("&Options...", "Display Options", self.OnOptions)))
+
+    def createMenuBar(self):
         menuBar = wx.MenuBar()
-        menu1 = wx.Menu()
-        openMenuItem = menu1.Append(-1, "&Open", "Copy in status bar")
-        self.Bind(wx.EVT_MENU, self.OnOpen, openMenuItem)
-        quitMenuItem = menu1.Append(-1, "&Quit", "Quit")
-        self.Bind(wx.EVT_MENU, self.OnCloseWindow, quitMenuItem)
-        menuBar.Append(menu1, "&File")
-        menu2 = wx.Menu()
-        copyItem = menu2.Append(-1, "&Copy", "Copy")
-        self.Bind(wx.EVT_MENU, self.OnCopy, copyItem)
-        cutItem = menu2.Append(-1, "C&ut", "Cut")
-        self.Bind(wx.EVT_MENU, self.OnCut, cutItem)
-        pasteItem = menu2.Append(-1, "Paste", "Paste")
-        self.Bind(wx.EVT_MENU, self.OnPaste, pasteItem)
-        menuBar.Append(menu2, "&Edit")
+        for eachMenuData in self.menuData():
+            menuLabel = eachMenuData[0]
+            menuItem = eachMenuData[1:]
+            menuBar.Append(self.createMenu(menuItem), menuLabel)
         self.SetMenuBar(menuBar)
 
-        static = wx.StaticText(panel, wx.NewId(), "First Name",
-                pos=(10, 50))
+    def createMenu(self, MenuData):
+        menu = wx.Menu()
+        for eachLabel, eachStatus, eachHandler in MenuData:
+            if not eachLabel:
+                menu.AppendSeparator()
+                continue
+            menuItem = menu.Append(-1, eachLabel, eachStatus)
+            self.Bind(wx.EVT_MENU, eachHandler, menuItem)
+        return menu
+
+    def buttonData(self):
+        return (("FIRST",   self.OnFirst),
+                ("<< PREV", self.OnPrev),
+                ("NEXT >>", self.OnNext),
+                ("LAST",    self.OnLast))
+
+    def createButtonBar(self, panel, yPos=0):
+        xPos = 0
+        for eachLabel, eachHandler in self.buttonData():
+            pos = (xPos, yPos)
+            button = self.buildOneButton(panel, eachLabel, eachHandler, pos)
+            xPos += button.GetSize().width
+
+    def buildOneButton(self, parent, label, handler, pos=(0,0)):
+        button = wx.Button(parent, -1, label, pos)
+        self.Bind(wx.EVT_BUTTON, handler, button)
+        return button
+
+    def textFieldData(self):
+        return (("First Name", (10, 50)),
+                ("Last Name",  (10, 80)))
+
+    def createTextFields(self, panel):
+        for eachLabel, eachPos in self.textFieldData():
+            self.createCaptionedText(panel, eachLabel, eachPos)
+
+    def createCaptionedText(self, panel, label, pos):
+        static = wx.StaticText(panel, wx.NewId(), label, pos=pos)
         static.SetBackgroundColour("White")
-        text = wx.TextCtrl(panel, wx.NewId(), "", size=(100, -1),
-                pos=(80, 50))
-
-        static2 = wx.StaticText(panel, wx.NewId(), "Last Name",
-                pos=(10, 80))
-        static2.SetBackgroundColour("White")
-        text2 = wx.TextCtrl(panel, wx.NewId(), "", size=(100, -1),
-                pos=(80, 80))
-
-        firstButton = wx.Button(panel, -1, "FIRST")
-        self.Bind(wx.EVT_BUTTON, self.OnFirst, firstButton)
-
-        menu2.AppendSeparator()
-        optItem = menu2.Append(-1, "&Options...", "Display Options")
-        self.Bind(wx.EVT_MENU, self.OnOptions, optItem)
-
-        lastButton = wx.Button(panel, -1, "LAST", pos=(240, 0))
-        self.Bind(wx.EVT_BUTTON, self.OnLast, lastButton)
-
+        textPos = ((pos[0] + 75), pos[1])
+        wx.TextCtrl(panel, wx.NewId(), "", size=(100, -1), pos=textPos)
 
     # Just grouping the empty event handlers together
     def OnPrev(self, event): pass
