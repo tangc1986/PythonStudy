ae4555e98bc3c5773cacde1b171bb2d1d29e0979
tangc1986 <tangc1986@gmail.com>
2016-05-01 11:18:50 +0800

    2016.05.01

---
+++ diveIntoPython/plural/plural2.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'tangchao'
+
+
+import re
+
+
+def match_sxz(noun):
+    return re.search('[sxz]$', noun)
+
+def apply_sxz(noun):
+    return re.sub('$', 'es', noun)
+
+def match_h(noun):
+    return re.search('[^aeioudgkprt]h$', noun)
+
+def apply_h(noun):
+    return re.sub('$', 'es', noun)
+
+def match_y(noun):
+    return re.search('[^aeiou]y$', noun)
+
+def apply_y(noun):
+    return re.sub('y$', 'ies', noun)
+
